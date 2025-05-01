5c6c82ef7dcd3c730305383f0883047a5a0969b9
mehdi-ra <mehdirahimi.dev@gmail.com>
2025-05-01 22:37:41 +0330

    feat: sample files added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use gpui::Application;
+mod components;
+
+use components::Assets;
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::new().with_assets(Assets);
+
+    app.run(move |cx| {
+        cx.activate(true);
+    })
 }
